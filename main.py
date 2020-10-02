b734167588cd8d477b81a8e13876347f2ad18eff
tedtessa <ttessa@student.bridgew.edu>
2020-10-01 23:51:43 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import arcade
+import random
+
+def main():
+    shapes_data =["100,200,100",
+                  "200,200,200",
+                  "300,200,250",
+                  "400,200,175",
+                  "300,250,200"]
+    our_colors =[arcade.color.REDWOOD,
+                 arcade.color.DEEP_SKY_BLUE,
+                 arcade.color.APPLE_GREEN,
+                 arcade.color.BABY_PINK,
+                 arcade.color.BRITISH_RACING_GREEN]
+    arcade.open_window(700, 700, "string split demo")
+
+    arcade.start_render()
+    for line in shapes_data:
+        data = line.split(",")
+        current_color = random.choice(our_colors)
+        bottom_y = int(data[1])
+        height = int(data[2])
+        center_y = bottom_y + height/2
+        arcade.draw_rectangle_filled(int(data[0]),center_y ,
+                                     50, int(data[2]), current_color)
+
+
+
+    arcade.finish_render()
+
+
+    arcade.run()
+
+
+main()
